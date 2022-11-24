853a40e1328812d1ac85c371b5c0e3f77f768bc9
ParkChanhee1130 <kkazza1130@gmail.com>
2022-11-24 15:36:40 +0900

    cm

--- docs/browser_js/js/javascriptWithPoll.js
+++ docs/browser_js/js/javascriptWithPoll.js
@@ -63,27 +63,85 @@ const questions_answers = [
 
 // 1차 방식 : [Q1, Q2, Q3, Q4, Q5]
 // 2차 방식 : Array in Array [[Q1, E1, E2], [Q2, E1, E2, E3]...]]
-let polls = []; // 전체 묶음
-let question_compare;
-let questions = []; // 내부 묶음
+// let polls = []; // 전체 묶음
+// let question_compare;
+// let questions = []; // 내부 묶음
 
+// for (let idx = 0; idx < questions_answers.length; idx++) {
+//   if (question_compare != questions_answers[idx]["questions_uid"]) {
+//     if (questions.length > 0) {
+//       polls.push(questions);
+//       questions = [];
+//     }
+//     // console.log(`!== : ${questions_answers[idx]["questions_uid"]}`);
+//     // console.log(`!== : ${questions_answers[idx]["answer_uid"]}`);
+//     questinos.push(questions_answers[idx]["questions_uid"]);
+//     questinos.push(questions_answers[idx]["answer_uid"]);
+//   } else {
+//     // console.log(`== : ${questions_answers[idx]["answer_uid"]}`);
+//     questinos.push(questions_answers[idx]["answer_uid"]);
+//   }
+//   if (idx + 1 >= questions_answers.length) {
+//     polls.push(questions);
+//   }
+//   question_compare = questions_answers[idx]["questions_uid"]; // 이전 uid 입력
+// }
+// console.log(`${polls}`);
+
+let polls = [];
+let question_compare;
+let questions = {}; // 내부 묶음
+let answer_uids = []; // 내부 설문 답변 묶음
 for (let idx = 0; idx < questions_answers.length; idx++) {
   if (question_compare != questions_answers[idx]["questions_uid"]) {
-    if (questions.length > 0) {
+    if (Object.keys(questions).length > 0) {
+      questions["answer_uids"] = answer_uids;
       polls.push(questions);
-      questions = [];
+      questions = {};
+      answer_uids = [];
     }
-    // console.log(`!== : ${questions_answers[idx]["questions_uid"]}`);
-    // console.log(`!== : ${questions_answers[idx]["answer_uid"]}`);
-    questinos.push(questions_answers[idx]["questions_uid"]);
-    questinos.push(questions_answers[idx]["answer_uid"]);
+
+    // console.log(`!= : ${questions_answers[idx]["questions_uid"]}`);
+    // console.log(`!= : ${questions_answers[idx]["answer_uid"]}`);
+    questions["questions_uid"] = questions_answers[idx]["questions_uid"];
+    answer_uids.push(questions_answers[idx]["answer_uid"]);
   } else {
     // console.log(`== : ${questions_answers[idx]["answer_uid"]}`);
-    questinos.push(questions_answers[idx]["answer_uid"]);
-  }
-  if (idx + 1 >= questions_answers.length) {
-    polls.push(questions);
+    answer_uids.push(questions_answers[idx]["answer_uid"]);
+    if (idx + 1 >= questions_answers.length) {
+      questions["answer_uids"] = answer_uids;
+      polls.push(questions);
+    }
   }
   question_compare = questions_answers[idx]["questions_uid"]; // 이전 uid 입력
 }
-console.log(`${polls}`);
+// console.log(`${polls}`);
+
+// 출력
+// polls[0]["questions_uid"];
+// polls[0]["answer_uid"][0];
+// polls[0]["answer_uid"][1];
+
+// polls[1]['questions_uid']
+// polls[1]['answer_uid'][0];
+
+// 설문 문항을 가져오는 function
+function getQuestionByUid(questions_uid) {
+  let questions_desc;
+  for (list of questions_list) {
+    if (list["questions_uid"] == questions_uid) {
+      questions_desc = list["question"];
+    }
+  }
+  return questions_desc;
+}
+
+for (poll of polls) {
+  console.log(
+    `${poll["questions_uid"]}. ${getQuestionByUid(poll["questions_uid"])}`
+  ); // ==polls[idx]
+  let answer_uids = poll["answer_uids"];
+  answer_uids.forEach((answer_uid, index) => {
+    console.log(`${index + 1}. :  ${answer_uid}`);
+  });
+}
